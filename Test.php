24e94a11a8a88371ebd2338832dee672e4c05a27
Riyad <riyadislam16@gmail.com>
2021-09-27 12:13:53 +0600

    commited

---
+++ Test.php
@@ -0,0 +1,40 @@
+<?php
+
+$message ="";
+$message1="";
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+    $FIRST=$_POST["username"];
+    $LAST=$_POST["name"];
+    
+  
+    
+if (empty ($FIRST)){
+    $message ="Please try again";
+   
+}
+else
+ {
+     $message=$FIRST;
+ }
+     
+if (empty ($LAST)){
+    $message1 ="Please try again";
+   
+}
+else
+ {
+     $message1=$LAST;
+ }
+
+  
+
+}
+
+
+
+
+
+
+
+?>
---
+++ Try.php
@@ -0,0 +1,38 @@
+<?php include "Test.php";
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTml</title>
+</head>
+<body>
+    <form action=" " method="POST">
+      First Name:   <input type="text" name="username"><br>
+      <?php
+      echo $message;
+      ?>
+      Last  Name:   <input type="text" name="name">
+      <?php 
+      echo $message1;
+       ?>
+      
+      Age:          <input type="number" name="age"><br>
+      Designation: 
+      <input type="radio" name="programmer" value="Junior">Junior Programmer
+<input type="radio" name="programmer" value="Senior">Senior Programmer
+<input type="radio" name="programmer" value="Project">Project Lead
+<br>
+ Preferred Language  <input type="checkbox" name="language">JAVA
+<input type="checkbox" name="language">PHP
+<input type="checkbox" name="language">c++
+<br>
+ <input type ="Submit"> 
+ 
+<input type="reset">
+
+</body>
+</html>
